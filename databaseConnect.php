8db9f9a34840de9fd3ce2adbc3d403a86b6b01d1
Ed Hall <edward.hall.144@gmail.com>
2020-02-24 12:59:02 +0000

    request_log table created automatically

--- databaseConnect.php
+++ databaseConnect.php
@@ -6,7 +6,8 @@
         die($mysqli->connect_errno . ': ' . $mysqli->connect_error);
     }
 
-    //Create relevant table
+    //Create relevant tables
     $mysqli->query("CREATE TABLE IF NOT EXISTS notifications_log(id INT auto_increment PRIMARY KEY, json JSON)");
+    $mysqli->query("CREATE TABLE IF NOT EXISTS requests_log(id INT auto_increment PRIMARY KEY, month VARCHAR(255), counter INT DEFAULT 0)");
     
 ?>
